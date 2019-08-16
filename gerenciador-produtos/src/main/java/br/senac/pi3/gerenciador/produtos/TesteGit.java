3dd8e062c3b5eb298ce94615cd38a6aab22e9f91
milenasg91@gmail.com <>
2019-08-15 21:47:37 -0300

    Configuração inicial POM.XML

---
+++ gerenciador-produtos/src/main/java/br/senac/pi3/gerenciador/produtos/TesteGit.java
@@ -0,0 +1,16 @@
+
+package br.senac.pi3.gerenciador.produtos;
+
+/**
+ *
+ * @author milena.sgalvao
+ */
+public class TesteGit {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Teste commits");
+    }
+    
+    
+}
